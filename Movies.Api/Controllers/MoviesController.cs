f286bc00c699c0d9ff47ece2e6395fd778b6732e
Mohammad Asad Mohammad <mmohammadq8@outlook.com>
2024-03-09 10:40:45 +0300

    init

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Models;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class MoviesController(IMovieRepository movieRepository) : ControllerBase
+{
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+        await movieRepository.CreateAsync(movie);
+        return CreatedAtAction(nameof(Get), new { id = movie.Id }, movie);
+    }
+
+    [HttpGet(ApiEndpoints.Movies.Get)]
+    public async Task<IActionResult> Get([FromRoute] Guid id)
+    {
+        var movie = await movieRepository.GetByIdAsync(id);
+        if (movie == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(movie.MapToResponse());
+    }
+
+    [HttpGet(ApiEndpoints.Movies.GetAll)]
+    public async Task<IActionResult> GetAll()
+    {
+        var movies = await movieRepository.GetAllAsync();
+        var moviesResponse = movies.MapToResponse();
+        return Ok(moviesResponse);
+    }
+
+    [HttpPut(ApiEndpoints.Movies.Update)]
+    public async Task<IActionResult> Update([FromRoute]Guid id, [FromBody]UpdateMovieRequest request)
+    {
+        var movie = request.MapToMovie(id);
+        var updated = await movieRepository.UpdateAsync(movie);
+        if (!updated)
+            return NotFound();
+        return Ok(movie.MapToResponse());
+    }
+    
+    
+}
---
+++ Movies.Api/Program.cs
@@ -0,0 +1,28 @@
+using Movies.Application;
+using Movies.Application.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
