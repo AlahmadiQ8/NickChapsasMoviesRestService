fa5d91c0c4b7635c0c89ff6210accf2c3d40a902
Mohammad Asad Mohammad <mmohammadq8@outlook.com>
2024-04-04 17:44:31 +0300

    uj

---
+++ Movies.Api/Auth/AdminAuthRequirement.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Movies.Api.Auth;
+
+public class AdminAuthRequirement(string apiKey) : IAuthorizationHandler, IAuthorizationRequirement
+{
+    public Task HandleAsync(AuthorizationHandlerContext context)
+    {
+        if (context.User.HasClaim(AuthConstants.AdminUserClaimName, "true"))
+        {
+            context.Succeed(this);
+            return Task.CompletedTask;
+        }
+
+        var httpContext = context.Resource as HttpContext;
+
+        if (httpContext is null)
+        {
+            return Task.CompletedTask;
+        }
+        
+        if (!httpContext.Request.Headers.TryGetValue(AuthConstants.ApiKeyHeaderName, out var extractedApiKey))
+        {
+            context.Fail();
+            return Task.CompletedTask;
+        }
+        
+        if (apiKey != extractedApiKey)
+        {
+            context.Fail();
+            return Task.CompletedTask;
+        }
+
+        var identity = (ClaimsIdentity)httpContext.User.Identity!;
+        identity.AddClaim(new Claim("userid", Guid.Parse("6c7uQMZuX9UHUWG4xVOt/nZlakXKFRXdV2aDHVwNAnQ=").ToString()));
+        context.Succeed(this);
+        return Task.CompletedTask;
+    }
+}
